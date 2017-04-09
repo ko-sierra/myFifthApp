16aedff02e9e68bba6d657984f707b79b01dd8bb
清水光二 <koji22468234@icloud.com>
2017-04-09 19:08:37 +0900

    情報更新

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,42 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by 清水光二 on 2017/04/09.
+//  Copyright © 2017年 koji. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+     var task: Task!   // 追加する
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 清水光二 on 2017/04/09.
+//  Copyright © 2017年 koji. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    /// 日時
+    dynamic var date = NSDate()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
